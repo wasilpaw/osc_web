5f17e03e82d1ca083edb93991c883f8f93c6f291
Pawel Wasilewski <35202418+wasilpaw@users.noreply.github.com>
2022-05-29 08:47:06 +0100

    Hago theme added

---
+++ public/js/active.js
@@ -0,0 +1,30 @@
+(function ($) {
+    'use strict';
+
+    var $window = $(window);
+
+    // Sticky Active Code
+    $window.on('scroll', function () {
+        if ($window.scrollTop() > 20) {
+            $('.header-area').addClass('sticky');
+        } else {
+            $('.header-area').removeClass('sticky');
+        }
+        if ($('#post-toc').length > 0) {
+            if ($window.scrollTop() > 157) {
+                $('.post-sidebar-area').addClass('sticky');
+            } else {
+                $('.post-sidebar-area').removeClass('sticky');
+            }
+        }
+    });
+
+    // ScrollUp
+    if ($.fn.scrollUp) {
+        $.scrollUp({
+            scrollSpeed: 1000,
+            easingType: 'easeInOutQuart',
+            scrollText: '<i class="fa fa-angle-up" aria-hidden="true"></i>'
+        });
+    }
+})(jQuery);
